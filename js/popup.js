f4b0253aab0677127b8bd5a105b18f893f6141aa
Denis <deniskaufa@ya.ru>
2018-08-13 01:10:13 +0300

    Add js-popup

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,8 @@
 var popupShow = document.querySelector(".hotel-search_btn");
 var popupBlock = document.querySelector(".hotel-search_modal");
 var startDay = document.querySelector("#start-day");
-
-
+var jsWork = document.querySelector(".no-js");
+jsWork.classList.replace("no-js", "js")
 popupShow.addEventListener("click", function(evn) {
     evn.preventDefault();
     popupBlock.classList.toggle("modal-block__show");
