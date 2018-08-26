573fbbbe755a5f8afd226b6ba72706afd7d85ed2
Denis <deniskaufa@ya.ru>
2018-08-27 01:18:45 +0300

    inner page

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 "use stict";
 var popupShow = document.querySelector(".hotel-search_btn");
 var popupBlock = document.querySelector(".hotel-search_modal");
-var startDay = document.querySelector("#start-day");
+var startDay = document.querySelector("#start-day"); 
 var jsWork = document.querySelector(".no-js");
 jsWork.classList.replace("no-js", "js")
 popupShow.addEventListener("click", function(evn) {
@@ -9,3 +9,13 @@ popupShow.addEventListener("click", function(evn) {
     popupBlock.classList.toggle("modal-block__show");
     startDay.focus();
 });
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        
+        if (popupBlock.classList.contains("modal-block__show")) {
+            popupBlock.classList.remove("modal-block__show");
+        }
+    } 
+});
+                        
